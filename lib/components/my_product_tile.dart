b7438f027edcce5bdc301a1ad97aeb3816645761
John Dixon <dixonaws@amazon.com>
2023-12-28 17:30:31 -0500

    add to cart button

--- lib/components/my_product_tile.dart
+++ lib/components/my_product_tile.dart
@@ -1,28 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../models/product.dart';
+import '../models/shop.dart';
 
 class MyProductTile extends StatelessWidget {
   final Product product;
+
+  void addToCart(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text("Are you sure you want to add this item to the cart?"),
+        actions: [
+          // cancel button
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("Cancel"),
+          ),
+
+          // confirm button
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              context.read<Shop>().addItemToCart(product);
+            },
+            child: Text("Confirm"),
+          ),
+
+        ],
+      ),
+    );
+  }
 
   const MyProductTile({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(25),
+      width: 300,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // image
-          Icon(Icons.favorite),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // image
+              AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.all(25),
+                  width: double.infinity,
+                  child: Icon(Icons.favorite),
+                ),
+              ),
 
-          // name
-          Text(product.name),
+              SizedBox(
+                height: 25,
+              ),
 
-          // desc
-          Text(product.description),
+              // name
+              Text(
+                product.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
 
+              SizedBox(
+                height: 10,
+              ),
+
+              // desc
+              Text(
+                product.description,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+
+              SizedBox(
+                height: 25,
+              ),
+            ],
+          ),
           // price + add to cart button
-          Text(
-            product.price.toStringAsFixed(2),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('\$' + product.price.toStringAsFixed(2)),
+
+              // button
+              Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondary,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: IconButton(
+                      onPressed: () => addToCart(context),
+                      icon: Icon(Icons.add)))
+            ],
           ),
         ],
       ),
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -13,24 +13,39 @@ class ShopPage extends StatelessWidget {
     final products = context.watch<Shop>().shop;
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
-        elevation: 0,
-        title: Text('Shop'),
-      ),
-      drawer: MyDrawer(),
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          // get each individual item from the shop
-          final product = products[index];
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+          elevation: 0,
+          title: Text('Shop'),
+        ),
+        drawer: MyDrawer(),
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: ListView(
+          children: [
+            SizedBox(height: 25),
+            Center(
+                child: Text(
+              "Choose from a list of premium products",
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary),
+            )),
+            SizedBox(
+              height: 550,
+              child: ListView.builder(
+                padding: EdgeInsets.all(15),
+                itemCount: products.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  // get each individual item from the shop
+                  final product = products[index];
 
-          // return as a product tile
-          return (MyProductTile(product: product));
-        },
-      ),
-    );
+                  // return as a product tile
+                  return (MyProductTile(product: product));
+                },
+              ),
+            ),
+          ],
+        ));
   }
 }
